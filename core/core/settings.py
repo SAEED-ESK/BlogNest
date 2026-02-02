54e54d94ad24f0e654e5fd8388ebf801ea1dfe32
Saeed Eskandari <eskandary.saeed336@gmail.com>
2026-02-02 13:26:33 +0330

    Add cors headers

--- core/core/settings.py
+++ core/core/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "drf_yasg",
-    'mail_templated'
+    'mail_templated',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -158,3 +160,5 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
 
 CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+CORS_ALLOW_ALL_ORIGINS = True
