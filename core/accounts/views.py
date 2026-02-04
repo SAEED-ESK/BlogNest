1f11911a147c3accab42dcc8268fc84e89699940
Saeed Eskandari <eskandary.saeed336@gmail.com>
2026-02-04 11:23:12 +0330

    Add caching with redis

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.cache import cache_page
 from .tasks import sendEmail
 
+import requests
+
 def send_email(request):
+    """
+    A test task for testing celery
+    """
     sendEmail.delay()
     return HttpResponse('<h1>Done sending</h1>')
+
+@cache_page(60)
+def test(request):
+    """
+    A test func for testing cashing
+    """
+    url = "https://ba646c97-a0ed-428f-a9d0-567c27a591c7.mock.pstmn.io/test/delay/5"
+    response = requests.get(url)
+    return JsonResponse(response.json())
--- core/core/settings.py
+++ core/core/settings.py
@@ -159,6 +159,19 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
 
+# Celery configs
 CELERY_BROKER_URL = 'redis://redis:6379/1'
 
+# Cors headers configs
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Cashing Configs
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
